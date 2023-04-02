d8f3d1410dec9a1b5eaaa005e60f38f878a9af51
zeyf <zainy0012@gmail.com>
2023-04-01 20:29:15 -0400

    Removed the frontend files / folders from the backend branch -- opting for a frontend and backend branch instead that will be pulled into a testing branch before PR and MR into main.

--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/question/body/topics/Interfaces.ts
+++
@@ -1,7 +0,0 @@
-export interface topicInterface {
-    topic: string;
-};
-
-export interface topicsInterface {
-    topics: string[];
-};
--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/question/header/Interfaces.ts
+++
@@ -1,9 +0,0 @@
-export interface headerInterface {
-    questionTitle: string;
-    questionIndex: number;
-    questionDifficulty: string;
-};
-
-export interface difficultyRankingInterface {
-    questionDifficulty: string;
-};
