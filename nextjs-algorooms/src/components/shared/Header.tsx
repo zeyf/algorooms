5ea812939e19e128250ec57727a129c27a47ea78
zeyf <zainy0012@gmail.com>
2023-04-03 19:44:49 -0400

    Added the flow for the login for the Navbar for the sign in adn sign out, also implemented profile

--- nextjs-algorooms/src/components/shared/Header.tsx
+++ nextjs-algorooms/src/components/shared/Header.tsx
@@ -4,6 +4,7 @@ import { Navbar } from "flowbite-react";
 import  Image from 'next/image';
 import Logo from '../images/AlgoRoomsLogo.png';
 import { IoPersonCircleOutline } from "react-icons/io5"
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 // Interface imports
 import { headerInterface } from "./Interfaces";
@@ -14,12 +15,26 @@ const Header = ({
 
     // Code
 
+    const { user, isLoading, error } = useUser();
+
     return (
         <Navbar fluid={true} rounded={true} className="w-screen h-15 bg-navbar drop-shaddow-[4px]">
             <Image alt="Logo" src={Logo} width={120} height={120}/>
             <div className="flex flex-row space-x-7 items-center">
-                <Navbar.Link href="/api/auth/login" className="text-white -translate-y-3" >Sign In / Sign Up</Navbar.Link>
-                <IoPersonCircleOutline className="text-white w-6 h-6"></IoPersonCircleOutline>
+
+                {
+                    user ?
+                    <Navbar.Link href="/api/auth/logout" className="text-white -translate-y-3" >Sign Out</Navbar.Link>
+                    :
+                    <Navbar.Link href="/api/auth/login" className="text-white -translate-y-3" >Sign In</Navbar.Link>
+                }
+                
+                {
+                    isLoading || !user ? 
+                    <IoPersonCircleOutline className="text-white w-6 h-6"></IoPersonCircleOutline>
+                    :
+                    <img className="text-white w-6 h-6 rounded-full" alt="user" src={isLoading || !user ? "" : user.picture || ""} width={120} height={120}/>
+                }
             </div>
         </Navbar>
     );
--- nextjs-algorooms/src/pages/index.tsx
+++ nextjs-algorooms/src/pages/index.tsx
@@ -1,10 +1,13 @@
 import { io } from "socket.io-client"
 import LoginButton from "../components/pages/home/button";
 import Header from "../components/shared/Header";
+// import { withApiAuthRequired } from '@auth0/nextjs-auth0';
+// import { useUser } from '@auth0/nextjs-auth0/client';
 
-const socket = io('http://localhost:3000')
+// const socket = io('http://localhost:3000')
 
 export default () => {
+
   return (
     <div className="bg-gradient-to-tr from-navbar to to-[#24366c] w-screen h-screen">
       <Header/>
