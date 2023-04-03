ebe35884f425d67d141c3d1095b283c0be65fe7f
Khoi Vu <khoivu@Khois-MacBook-Pro.local>
2023-04-03 13:37:45 -0400

    removed buttons due to Oauth

--- nextjs-algorooms/src/pages/index.tsx
+++ nextjs-algorooms/src/pages/index.tsx
@@ -6,9 +6,7 @@ const socket = io('http://localhost:3000')
 export default () => {
   return (
     <div className="bg-[#222C4A] w-screen h-screen flex justify-center items-center">
-      <div className="bg-white w-[565px] h-[647px] rounded-lg flex justify-center items-center">
-        <LoginButton/>
-      </div>
+      
     </div>
   )
 }
