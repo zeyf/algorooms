ba152d47f8a88ba5b42684d0ad5f9dc05743650e
johnhuber3 <johnrhuber001@gmail.com>
2023-04-03 14:29:27 -0400

    Made the base version of the navbar

--- nextjs-algorooms/src/components/shared/Header.tsx
+++ nextjs-algorooms/src/components/shared/Header.tsx
@@ -1,19 +1,28 @@
 // Module imports
 import React from "react";
+import { Navbar, Button } from "flowbite-react";
+import { Nav, Container } from 'react-bootstrap';
 
 // Interface imports
 import { headerInterface } from "./Interfaces";
 
-export default ({
+const Header = ({
     
 }:headerInterface) => {
 
     // Code
 
     return (
-        <nav>
-            {/* Body */}
-        </nav>
+        <Navbar fluid={true} rounded={true} className="w-screen bg-[#051135] drop-shaddow-{4px}">
+            <Navbar.Brand href="/algorooms/nextjs-algorooms/src/pages/index.tsx">
+                <span className="self-center whitespace-nowrap text-xl font-semibold text-green-500 dark:text-white">Algorooms</span>
+            </Navbar.Brand>
+            <Navbar.Collapse>
+                <Navbar.Link href="/algorooms/nextjs-algorooms/src/pages/index.tsx" className="text-green-500">Rooms</Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
     );
 
 };
+
+export default Header;
--- nextjs-algorooms/src/pages/index.tsx
+++ nextjs-algorooms/src/pages/index.tsx
@@ -1,12 +1,13 @@
 import { io } from "socket.io-client"
 import LoginButton from "../components/pages/home/button";
+import Header from "../components/shared/Header";
 
 const socket = io('http://localhost:3000')
 
 export default () => {
   return (
-    <div className="bg-[#222C4A] w-screen h-screen flex justify-center items-center">
-      
+    <div className="bg-[#222C4A] w-screen h-screen">
+      <Header></Header>
     </div>
   )
 }
