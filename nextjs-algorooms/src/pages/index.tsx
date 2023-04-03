ff0256b653398683cf2a8a357779b963a2963f89
Khoi Vu <khoivu@Khois-MacBook-Pro.local>
2023-04-03 12:28:21 -0400

    Set background color and began login card

--- nextjs-algorooms/src/pages/index.tsx
+++ nextjs-algorooms/src/pages/index.tsx
@@ -4,6 +4,10 @@ const socket = io('http://localhost:3000')
 
 export default () => {
   return (
-    null
+    <div className="bg-[#222C4A] w-screen h-screen flex justify-center items-center">
+      <div className="bg-white w-[565px] h-[647px] rounded-lg ">
+
+      </div>
+    </div>
   )
 }
