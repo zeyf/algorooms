9fa3e68ee33e79176a4656cbb3c4373bee32bc7f
Khoi Vu <khoivu@Khois-MacBook-Pro.local>
2023-04-03 18:45:37 -0400

    removed the .env gitignore and added a gradient to the landing page

--- nextjs-algorooms/src/components/shared/Header.tsx
+++ nextjs-algorooms/src/components/shared/Header.tsx
@@ -1,6 +1,6 @@
 // Module imports
 import React from "react";
-import { Navbar, Button } from "flowbite-react";
+import { Navbar } from "flowbite-react";
 import  Image from 'next/image';
 import Logo from '../images/AlgoRoomsLogo.png';
 import { IoPersonCircleOutline } from "react-icons/io5"
@@ -15,8 +15,8 @@ const Header = ({
     // Code
 
     return (
-        <Navbar fluid={true} rounded={true} className="w-screen h-15 bg-navbar drop-shaddow-{4px}">
-            <Image alt="Logo" src={Logo} width={120} height={120} className=""/>
+        <Navbar fluid={true} rounded={true} className="w-screen h-15 bg-navbar drop-shaddow-[4px]">
+            <Image alt="Logo" src={Logo} width={120} height={120}/>
             <div className="flex flex-row space-x-7 items-center">
                 <Navbar.Link href="/api/auth/login" className="text-white -translate-y-3" >Sign In / Sign Up</Navbar.Link>
                 <IoPersonCircleOutline className="text-white w-6 h-6"></IoPersonCircleOutline>
--- nextjs-algorooms/src/pages/index.tsx
+++ nextjs-algorooms/src/pages/index.tsx
@@ -6,7 +6,7 @@ const socket = io('http://localhost:3000')
 
 export default () => {
   return (
-    <div className="bg-[#222C4A] w-screen h-screen">
+    <div className="bg-gradient-to-tr from-navbar to to-[#24366c] w-screen h-screen">
       <Header/>
     </div>
   )
