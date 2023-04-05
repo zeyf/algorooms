fad4e6e112cbc1b2a7e1bf6fc31489ff1bcda218
johnhuber3 <johnrhuber001@gmail.com>
2023-04-05 19:53:23 -0400

    Got rid of the flowbite Card and made a new one from scratch

--- nextjs-algorooms/src/components/pages/rooms/CreateRoomCard.tsx
+++ nextjs-algorooms/src/components/pages/rooms/CreateRoomCard.tsx
@@ -4,21 +4,21 @@ import { Card, Label, Checkbox, Button, Dropdown } from "flowbite-react";
 const CreateRoomCard = () => {
   return (
     <div className="w-[537px] h-[713px]">
-      <Card className="h-full flex flex-col">
+      <div className="h-full flex flex-col rounded-2xl bg-white bg-opacity-25 shadow-xl">
         <form className="h-full flex flex-col justify-around p-10">
-          <div className="flex justify-center w-full">
+          <div className="flex justify-center w-full translate-y-[25px]">
             <div>
               <input
                 type="text"
                 placeholder="Room Name"
-                className="w-[348px] border-b border-gray-500 border-t-0 border-l-0 border-r-0"
+                className="w-[348px] bg-white bg-opacity-0 border-b border-white border-t-0 border-l-0 border-r-0 placeholder-white text-white"
               />
             </div>
           </div>
-          <p className="flex justify-center text-base text-gray-500 dark:text-gray-400 sm:text-lg translate-y-[-20px]">
+          <p className="flex justify-center text-base text-white dark:text-gray-400 sm:text-lg translate-y-[0px]">
             Select Number of People
           </p>
-          <div className="flex justify-center gap-[11px] translate-y-[-50px]">
+          <div className="flex justify-center gap-[11px] translate-y-[-35px]">
             <button type="button" className="bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black w-[50px] h-[50px] rounded font-bold">
               1
             </button>
@@ -38,8 +38,8 @@ const CreateRoomCard = () => {
               6
             </button>
           </div>
-          <div className="flex justify-center translate-y-[-45px] z-50">
-            <Dropdown label="Select Topics" className="" inline={true}>
+          <div className="flex justify-center translate-y-[-30px] z-50 text-white">
+            <Dropdown label="Select Topics" inline={true}>
               <Dropdown.Item>
               <Checkbox id="arrays" />
                 Arrays</Dropdown.Item>
@@ -67,11 +67,11 @@ const CreateRoomCard = () => {
           <button type="button" className="w-[150px] h-[60px] bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black font-bold py-2 px-4 rounded">Public</button>
           <button type="button" className="w-[150px] h-[60px] bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black font-bold py-2 px-4 rounded">Private</button>
           </div>
-          <div className="flex justify-center translate-y-[-20px]">
-            <button type="submit" className="bg-[#19F8A7] hover:bg-[#051135] hover:text-white text-black w-[200px] h-[61px] font-bold rounded">Create Room</button>
+          <div className="flex justify-center translate-y-[-40px]">
+            <button type="submit" className="bg-[#19F8A7] hover:bg-[#051135] hover:text-white text-black w-[200px] h-[60px] font-bold rounded">Create Room</button>
           </div>
         </form>
-      </Card>
+      </div>
     </div>
   );
 };
