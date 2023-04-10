75347a3f6b5bc7795dcae2c9cc60d08c698dbda8
James Nguyen <hjaenscode@gmail.com>
2023-04-10 19:57:05 -0400

    Restructured profile page and allowed google pfps

--- nextjs-algorooms/src/components/pages/profile/ProgressBar.tsx
+++ nextjs-algorooms/src/components/pages/profile/ProgressBar.tsx
@@ -1,32 +1,41 @@
 // Module imports
-import React from 'react'
+import React from 'react';
 
 // Interface
 interface progressBarInterface {
-  "backgroundColor": string,
-  "accentColor": string,
-  "numerator": number,
-  "denominator": number,
-  "width": string,
-  "height": string,
-};
-
-const ProgressBar = ({ backgroundColor, accentColor, numerator, denominator, width, height } : progressBarInterface) => {
-  
-  // Code
-  
-  const completionPercentage: number = Math.floor(numerator / denominator * 100);
-  const completionComplement = 100 - completionPercentage;
-  
-  return (
-    <div className={`w-${width} h-${height} ${backgroundColor} rounded-full`}>
-
-      <div
-        className={`w-full h-${height} ${accentColor}`}
-        style={{ width: `${completionComplement}%`}}
-      ></div>
-    </div>
-  )
+  backgroundColor: string;
+  accentColor: string;
+  numerator: number;
+  denominator: number;
+  width: string;
+  height: string;
 }
 
-export default ProgressBar
+const ProgressBar = ({
+  backgroundColor,
+  accentColor,
+  numerator,
+  denominator,
+  width,
+  height,
+}: progressBarInterface) => {
+  // Code
+
+  const completionPercentage: number = Math.floor(
+    (numerator / denominator) * 100
+  );
+  const completionComplement = 100 - completionPercentage;
+
+  return (
+    <div
+      className={`w-${width} h-${height} ${backgroundColor} rounded-full overflow-hidden`}
+    >
+      <div
+        className={`w-full h-${height} ${accentColor}`}
+        style={{ width: `${completionComplement}%` }}
+      ></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- nextjs-algorooms/src/pages/profile/[profileUID]/index.tsx
+++ nextjs-algorooms/src/pages/profile/[profileUID]/index.tsx
@@ -1,139 +1,151 @@
-import Image from "next/image";
-import ProgressBar from "@/components/pages/profile/ProgressBar";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import Image from 'next/image';
+import ProgressBar from '@/components/pages/profile/ProgressBar';
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Subtitle from '@/components/pages/profile/Subtitle';
+import Title from '@/components/pages/profile/Title';
 
 interface DynamicProfilePageProps {
-    profileExists: boolean,
-    profileUID: string,
-    profileData: {  }
-};
+  profileExists: boolean;
+  profileUID: string;
+  profileData: {};
+}
 
 const DynamicProfilePage = ({
-    profileExists, profileUID, profileData
-}:DynamicProfilePageProps) => {
+  profileExists,
+  profileUID,
+  profileData,
+}: DynamicProfilePageProps) => {
+  if (!profileExists) {
+    return <h1>Profile Does Not Exist!</h1>;
+  }
 
-    if (!profileExists) {
-        return <h1>Profile Does Not Exist!</h1>
-    }
+  const currExp = 1100,
+    maxExp = 6100;
 
-    const currExp = 1100, maxExp = 6100;
-    
-    const {
-        user,
-        isLoading,
-        error
-    } = useUser();
-    
-    if (isLoading) {
-        <p>Loading...</p>
-    } else if (!user)
-        return <p>WTF!</p>
-    else 
-        return (
-            <div className="grid auto-row-fr grid-cols-2">
-            <div className="flex justify-between items-center col-span-2">
-            {/* User */}
-            <div className="flex-col w-1/3 p-2 h-full">
-                {/* Avatar and User Info */}
-                <div>
-                <div className="flex justify-between">
-                    {/* Avatar */}
-                    <img
-                    className="rounded-full"
-                    src={user.picture || ""}
-                    height={188}
-                    width={188}
-                    alt=""
-                    />
-                    {/* User Info */}
-                    <div className="flex-col">
-                    <h1 className="text-lg">{user.nickname || ""}</h1>
-                    <h3 className="text-sm">Joined 23 March 2023</h3>
-                    </div>
-                </div>
-                </div>
-                {/* Exp bar */}
-                <div className="flex justify-between items-center">
-                {/* Level */}
-                <h3>{100}</h3>
-    
-                {/* Exp Bar */}
-                <ProgressBar backgroundColor={"bg-green-300"} accentColor={"bg-green-800"} numerator={currExp} denominator={maxExp} width={"full"} height={"3"}/>
-                </div>
-            </div>
-    
-            {/* Problems */}
-            <div className="w-1/3 p-2 h-full">
-                <h3>Problems</h3>
-                <div className="flex-col justify-between w-full h-full">
-                {/* Simpler */}
-                <section className="flex flex-col items-start">
-                    <span className="font-bold my-2">Simpler</span>
-                    <ProgressBar backgroundColor={"bg-green-300"} accentColor={"bg-green-800"}
-                        numerator={5} denominator={25}
-                        width={"full"} height={"4"}
-                    />
+  const { user, isLoading, error } = useUser();
 
-                </section>
-                <section className="flex flex-col items-start">
-                    <span className="font-bold my-2">Simple</span>
-                    <ProgressBar backgroundColor={"bg-yellow-300"} accentColor={"bg-yellow-800"}
-                        numerator={5} denominator={25}
-                        width={"full"} height={"4"}
-                    />
-
-                </section>
-                
-                <section className="flex flex-col items-start">
-                    <span className="font-bold my-2">Not Simple</span>
-                    <ProgressBar backgroundColor={"bg-red-300"} accentColor={"bg-red-800"}
-                        numerator={5} denominator={25}
-                        width={"full"} height={"4"}
-                    />
-
-                </section>
-                </div>
-            </div>
-    
-            {/* Topics */}
-            <div className="w-1/3 p-2 h-full">
-                Topics
-            </div>
-            </div>
-            
-            {/* Statis */}
+  if (isLoading) {
+    <p>Loading...</p>;
+  } else if (!user) return <p>WTF!</p>;
+  else
+    return (
+      <div className="grid auto-row-fr grid-cols-3 p-6">
+        {/* First row */}
+        <div className="flex justify-between items-center col-span-full">
+          {/* User */}
+          <div className="flex-col w-1/2 p-2 h-full border-r-2">
+            {/* Avatar and User Info */}
             <div>
-            Statistics
+              <div className="flex justify-between">
+                {/* Avatar */}
+                <Image
+                  className="rounded-full"
+                  src={user.picture || ''}
+                  height={188}
+                  width={188}
+                  alt=""
+                />
+                {/* User Info */}
+                <div className="flex-col">
+                  <Title text={user.nickname || ''} alignment="left" />
+                  <h3 className="text-sm">Joined 23 March 2023</h3>
+                </div>
+              </div>
             </div>
-    
-            {/* Rating */}
-            <div>
-            Rating
+            {/* Exp bar */}
+            <div className="flex justify-between items-center">
+              {/* Level */}
+              <h3>{100}</h3>
+
+              {/* Exp Bar */}
+              <ProgressBar
+                backgroundColor={'bg-green-300'}
+                accentColor={'bg-green-800'}
+                numerator={currExp}
+                denominator={maxExp}
+                width={'full'}
+                height={'3'}
+              />
             </div>
+          </div>
+
+          {/* Problems */}
+          <div className="w-1/2 p-2 h-full">
+            <Title text="Problems" alignment="left" />
+            <div className="flex-col justify-between w-full h-full">
+              {/* Simpler */}
+              <section className="flex flex-col items-start">
+                <Subtitle text="Simpler" alignment="left" />
+                <ProgressBar
+                  backgroundColor={'bg-green-300'}
+                  accentColor={'bg-green-800'}
+                  numerator={5}
+                  denominator={25}
+                  width={'full'}
+                  height={'4'}
+                />
+              </section>
+              <section className="flex flex-col items-start">
+                <span className="font-bold my-2">Simple</span>
+                <ProgressBar
+                  backgroundColor={'bg-yellow-300'}
+                  accentColor={'bg-yellow-800'}
+                  numerator={5}
+                  denominator={25}
+                  width={'full'}
+                  height={'4'}
+                />
+              </section>
+
+              <section className="flex flex-col items-start">
+                <span className="font-bold my-2">Not Simple</span>
+                <ProgressBar
+                  backgroundColor={'bg-red-300'}
+                  accentColor={'bg-red-800'}
+                  numerator={5}
+                  denominator={25}
+                  width={'full'}
+                  height={'4'}
+                />
+              </section>
+            </div>
+          </div>
         </div>
-        );
 
+        {/* Second row */}
+        {/* Recently Solved Problems */}
+        <div>
+          <Title text="Recently Solved Problems" alignment="center" />
+        </div>
+
+        {/* Top Topics Solved */}
+        <div>
+          <Title text="Top Topics Solved" alignment="center" />
+        </div>
+
+        {/* School Affiliation */}
+        <div>
+          <Title text="School Affiliation" alignment="center" />
+        </div>
+      </div>
+    );
 };
 
-export async function getServerSideProps(data:any) {
+export async function getServerSideProps(data: any) {
+  const {
+    query: { profileUID },
+  } = data;
 
-    const {
-        query: {
-            profileUID
-        }
-    } = data;
+  // const response = await fetch(`localhost:4000/api/profile/search/${profileUID}`).then((res:any) => {
+  //     return res;
+  // });
 
-    // const response = await fetch(`localhost:4000/api/profile/search/${profileUID}`).then((res:any) => {
-    //     return res;
-    // });
-
-    return {
-        props: {
-            profileExists: true,
-            profileUID
-        }
-    };
-
+  return {
+    props: {
+      profileExists: true,
+      profileUID,
+    },
+  };
 }
 
 export default DynamicProfilePage;
