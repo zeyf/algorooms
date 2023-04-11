8eb372fbac7118b29951d3f7418103dfdbda8d13
zeyf <zainy0012@gmail.com>
2023-04-11 19:27:49 -0400

    Fixed state data modeling to be joined into an object as opposed 3 separate state/function pairs. Added basic logic to allow for visual button changes (such as background color and text color) upon selection through state and conditional rendering for capacity and access type. Also added headers. Added react-select's Select tag to add multiple selection of topics for a room as opposed to single topic selection for initial room question pool restrictions. Refactored code in order to prevent un-needed repetition.

--- nextjs-algorooms/src/components/pages/rooms/CreateRoomCard.tsx
+++ nextjs-algorooms/src/components/pages/rooms/CreateRoomCard.tsx
@@ -1,93 +1,115 @@
 import React from "react";
-import { Select, Option } from "@material-tailwind/react";
 import { Checkbox } from "@material-tailwind/react";
 import rooms from "./DummyRooms";
 import { useState } from "react";
 import JoinRoomCard from "./JoinRoomCard";
+import Select from "react-select";
+import { useRouter } from "next/router";
 
+const options = [
+  { value: "Strings", label: "Strings" },
+  { value: "Arrays", label: "Arrays" },
+  { value: "Stacks", label: "Stacks" },
+  { value: "Queues", label: "Queues" },
+  { value: "Linked Lists", label: "Linked Lists" },
+  { value: "Trees", label: "Trees" },
+  { value: "Tries", label: "Tries" },
+  { value: "Recursion", label: "Recursion" },
+  { value: "Hash Tables", label: "Hash Tables" },
+  { value: "Graphs", label: "Graphs" },
+  { value: "Bitwise", label: "Bitwise" }
+];
 
-type RoomJoinSectionProps = {
-  roomName: string;
-  topic: string;
-  occupied: number;
-  capacity: number;
-}
+export default ({
 
-const CreateRoomCard = (props:any) => {
+}) => {
 
-  const [topic, setTopic] = useState("")
-  const [roomName, setRoomName] = useState("");
-  const [capacity, setCapacity] = useState(0);
+  const [ data, setData ] = useState<any>({
+    selectedTopics: [],
+    selectedLobbyAccess: "Public",
+    capacity: 1
+  });
 
-  const addRoom = () => {
+  const router = useRouter();
+
+  const createRoom = async () => {
     // logic
-  }
+
+    // ...
+
+    router.push("/rooms/[roomUID]");
+  };
+
+  const setButtonColorOnCondition = (condition:boolean):string => condition ? "bg-darkAccent text-white" : "bg-greenAccent text-darkAccent";
 
   return (
-    <div className="w-[537px] h-[713px]">
+    <div className="w-[537px] h-[713px] overflow-y-scroll">
       <div className="h-full flex flex-col rounded-2xl bg-white bg-opacity-25 shadow-xl">
         <form className="h-full flex flex-col justify-around p-10">
-          <div className="flex justify-center w-full translate-y-[25px]">
-            <div>
-              <input
-                type="text"
-                placeholder="Room Name"
-                onChange={(event) => {
-                  setRoomName(event.target.value);
-                }}
-                className="w-[348px] bg-white bg-opacity-0 border-b border-white border-t-0 border-l-0 border-r-0 placeholder-white text-white"
-              />
-            </div>
-          </div>
           <p className="flex justify-center text-base text-white dark:text-gray-400 sm:text-lg translate-y-[0px]">
             Select Number of People
           </p>
           <div className="flex justify-center gap-[11px] translate-y-[-35px]">
-            <button type="button" onClick={() => setCapacity(1)} className="bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black w-[50px] h-[50px] rounded font-bold">
-              1
-            </button>
-            <button type="button" onClick={() => setCapacity(2)} className="bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black w-[50px] h-[50px] rounded font-bold">
-              2
-            </button>
-            <button type="button" onClick={() => setCapacity(3)} className="bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black w-[50px] h-[50px] rounded font-bold">
-              3
-            </button>
-            <button type="button" onClick={() => setCapacity(4)} className="bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black w-[50px] h-[50px] rounded font-bold">
-              4
-            </button>
-            <button type="button" onClick={() => setCapacity(5)} className="bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black w-[50px] h-[50px] rounded font-bold">
-              5
-            </button>
-            <button type="button" onClick={() => setCapacity(6)} className="bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black w-[50px] h-[50px] rounded font-bold">
-              6
-            </button>
+            {
+              [1,2,3,4,5,6].map(
+                (possibleCapacity:number) => (
+                  <button
+                    className={`${setButtonColorOnCondition(data.capacity === possibleCapacity)} w-[50px] h-[50px] rounded font-bold hover:opacity-75`}
+                    onClick={(event) => {
+                      event.preventDefault();
+                      setData({ ...data, capacity: possibleCapacity })
+                    }}
+                  >
+                    { possibleCapacity }
+                  </button>
+                )
+              )
+            }
           </div>
-          <div className="flex justify-center translate-y-[-40px] translate-x-[54px] z-50 text-white w-[348px]">
-            <Select variant="standard" label="Select Topics" className="border-white text-white">
-              <Option onClick={() => setTopic("Strings")}>Strings</Option>
-              <Option onClick={() => setTopic("Arrays")}>Arrays</Option>
-              <Option onClick={() => setTopic("Stacks")}>Stacks</Option>
-              <Option onClick={() => setTopic("Queues")}>Queues</Option>
-              <Option onClick={() => setTopic("Linked List")}>Linked List</Option>
-              <Option onClick={() => setTopic("Trees")}>Trees</Option>
-              <Option onClick={() => setTopic("Tries")}>Tries</Option>
-              <Option onClick={() => setTopic("Recursion")}>Recursion</Option>
-              <Option onClick={() => setTopic("Hash")}>Hash</Option>
-              <Option onClick={() => setTopic("Graphing")}>Graphing</Option>
-              <Option onClick={() => setTopic("Bitwise")}>Bitwise</Option>
-            </Select>
+          <div className="flex flex-col justify-center translate-y-[-40px] translate-x-[54px] z-50 text-white w-[348px]">
+            <p className="my-4 flex justify-center text-base text-white dark:text-gray-400 sm:text-lg translate-y-[0px]">
+              Select Topics
+            </p>
+            <Select
+              name="colors"
+              className="w-full basic-multi-select text-black"
+              classNamePrefix="select"
+              isMulti={true}
+              options={options}
+            />
           </div>
-          <div className="flex justify-center gap-[48px] translate-y-[-30px]">
-          <button type="button" className="w-[150px] h-[60px] bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black font-bold py-2 px-4 rounded">Public</button>
-          <button type="button" className="w-[150px] h-[60px] bg-[#19F8A7] hover:bg-[#051135] focus:bg-[#051135] active:bg-[#051135] focus:text-white active:text-white hover:text-white text-black font-bold py-2 px-4 rounded">Private</button>
+          <div className="flex flex-col justify-center gap-[48px] translate-y-[-30px]">
+            <p className="my-4 flex justify-center text-base text-white dark:text-gray-400 sm:text-lg translate-y-[0px]">
+              Select Lobby Access
+            </p>
+            <div>
+              <button
+                type="button"
+                className={`${setButtonColorOnCondition(data.selectedLobbyAccess === "Public")} w-[150px] h-[60px] mx-2 bg font-bold py-2 px-4 rounded`}
+                onClick={() => setData({ ...data, selectedLobbyAccess: "Public" })}
+                >
+                  Public
+                </button>
+              <button
+                type="button"
+                className={`${setButtonColorOnCondition(data.selectedLobbyAccess === "Private")} w-[150px] h-[60px] mx-2 bg font-bold py-2 px-4 rounded`}
+                onClick={() => setData({ ...data, selectedLobbyAccess: "Private" })}
+              >
+                Private
+              </button>
+            </div>
           </div>
           <div className="flex justify-center translate-y-[-40px]">
-            <button type="button" onClick={addRoom} className="bg-[#19F8A7] hover:bg-[#051135] hover:text-white text-black w-[200px] h-[60px] font-bold rounded">Create Room</button>
+            <button
+              type="button"
+              className="bg-greenAccent hover:bg-darkAccent hover:text-white text-black w-[200px] h-[60px] font-bold rounded"
+              onClick={createRoom}
+              >
+                Create Room
+              </button>
           </div>
         </form>
       </div>
     </div>
   );
 };
-
-export default CreateRoomCard;
