31b63cc3fcd5d53aca70a50bd621dd396f8702c0
johnhuber3 <johnrhuber001@gmail.com>
2023-04-13 19:09:40 -0400

    Moved the botton positioning

--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/header/Header.tsx
+++ nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/header/Header.tsx
@@ -18,25 +18,30 @@ export default ({
 
     return (
         <section>
-            <div className="w-[822px] flex flex-row space-x-5 items-center p-1">
-                <div className="w-[200px]">
-                    <Select label="Select Language" className="drop-shadow-lg" color="blue">
-                        <Option>Python</Option>
-                        <Option>Javascript</Option>
-                    </Select>
-                </div>
-
-                <Button href="/" color="dark" className="drop-shadow-lg">Run</Button>
-                <CountdownTimer />
-                
-                <button onClick={() => setIsSettingsOpen(!isSettingsOpen)} className="w-[71px] h-[46px] bg-darkAccent font-bold py-2 rounded-[15px] border-white border-[3px]">
-                    <FaCog className="text-white text-2xl ml-[21px]" />
-                </button>
-                {isSettingsOpen && (
-                    <div className="absolute z-50 translate-x-[300px] translate-y-[180px]">
-                        <SettingsPop />
+            <div className="w-[822px] flex flex-row space-x-5 items-center p-1 justify-between">
+                <div className="flex items-center gap-[21px]">
+                    <div className="w-[200px]">
+                        <Select label="Select Language" className="drop-shadow-lg" color="blue">
+                            <Option>Python</Option>
+                            <Option>Javascript</Option>
+                        </Select>
                     </div>
-                )}
+
+                    <Button href="/" color="dark" className="drop-shadow-lg">Run</Button>
+                    <Button href="/" color="dark" className="drop-shadow-lg">Submit</Button>
+                </div>
+                <div className="flex items-center gap-[21px]">
+                    <CountdownTimer />
+                    
+                    <button onClick={() => setIsSettingsOpen(!isSettingsOpen)} className="w-[71px] h-[46px] bg-darkAccent font-bold py-2 rounded-[15px] border-white border-[3px]">
+                        <FaCog className="text-white text-2xl ml-[21px]" />
+                    </button>
+                    {isSettingsOpen && (
+                        <div className="absolute z-50 translate-x-[-75px] translate-y-[195px]">
+                            <SettingsPop />
+                        </div>
+                    )}
+                </div>
             </div>
         </section>
     );
