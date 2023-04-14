cc5d0a8d9aed924a9b7d11c16a100e81cb0e287b
zeyf <zainy0012@gmail.com>
2023-04-14 17:24:12 -0400

    1. Added the rooms prop
    2. Destructured each room's properties in order to seed the mapped RoomJoinSection card component
    
    3. Passed all relevant room props to the RoomJoinSection component
    
    4. Added difficulty to the RoomJoinSection component
    
    5. Added the /rooms/[roomUID] Link href the room Join button

--- nextjs-algorooms/src/components/pages/rooms/JoinRoomCard.tsx
+++ nextjs-algorooms/src/components/pages/rooms/JoinRoomCard.tsx
@@ -2,63 +2,77 @@ import React, { useEffect } from "react";
 import { MdPeople } from "react-icons/md";
 import { useRouter } from "next/router";
 import CreateRoomCard from "./CreateRoomCard";
+import Link from "next/link";
 
 type RoomJoinSectionProps = {
-  roomName: string;
-  topic: string;
+  name: string;
+  topics: string[];
   occupied: number;
   capacity: number;
+  difficulty: string;
+  uid: string;
 }
 
-
-const rooms = [
-  {roomName: "CS Amigos", topic: "Stacks, Queues", occupied: 2, capacity: 4},
-  {roomName: "Code Amigos", topic: "Linked List", occupied: 3, capacity: 5},
-  {roomName: "Simple", topic: "Trees, Tries", occupied: 4, capacity: 6},
-  {roomName: "On Point", topic: "Recurssion", occupied: 2, capacity: 3},
-  {roomName: "Simple as well", topic: "Graphing, Hash", occupied: 1, capacity: 3},
-  {roomName: "Another Room", topic: "Another Topic", occupied: 2, capacity: 4},
-  {roomName: "Another Room", topic: "Another Topic", occupied: 2, capacity: 4},
-];
-
-const RoomJoinSection = ({roomName, topic, occupied, capacity}: RoomJoinSectionProps) => {
-  const router = useRouter()
+const RoomJoinSection = ({
+  name,
+  topics,
+  occupied,
+  capacity,
+  difficulty,
+  uid
+}: RoomJoinSectionProps) => {
+  const router = useRouter();
+  
   return (
     <div className="w-[452px] h-[77px] flex items-center rounded-xl border-black border-[4px] mx-11 mt-11 bg-white shadow-lg relative">
       <div className="flex flex-col">
-        <h2 className="ml-5 text-3xl">{roomName}</h2>
-        <p className="ml-5 mr-auto text-gray-500 text-lg">{topic}</p>
+        <h2 className="ml-5 text-3xl">{name}</h2>
+        <p className="ml-5 mr-auto text-gray-500 text-lg">{topics}</p>
       </div>
       <div className="absolute right-[165px]">
         <MdPeople className="text-black text-5xl mr-2" />
       </div>
+
+      <span>{ difficulty }</span>
+
       <div className="absolute right-[120px]">
         <h2 className="text-3xl ml-10">{occupied}/{capacity}</h2>
       </div> 
           {/* Remember to adjust element to include roomUID */}
-          <button onClick={() => {router.push("/rooms/roomUID")}} className="bg-[#19F8A7] hover:bg-[#051135] w-[100px] h-[56px] hover:text-white text-black font-bold rounded-xl ml-auto mr-2 text-2xl">
+          <Link
+            href={`/rooms/${uid}`}
+            className="bg-[#19F8A7] hover:bg-[#051135] w-[100px] h-[56px] hover:text-white text-black font-bold rounded-xl ml-auto mr-2 text-2xl"
+          >
             Join
-          </button>
+          </Link>
     </div>
   );
 };
 
-const JoinRoomCard = ({list}:any) => {
-  
+export default ({
+  rooms
+}:any) => {
 
   return (
     <div className="w-[567px] h-[713px] overflow-y-auto shadow-xl" style={{ scrollSnapType: 'y mandatory' }}>
       <div className="h-full flex flex-col rounded-2xl bg-white bg-opacity-25 drop-shadow-xl divide-y divide-gray-300 overflow-y-auto pb-[30px]  ">
         <div className="flex flex-col items-center justify-center flex-grow">
-          {rooms.map((rooms:any, index:any) => (
-            <div key={index} style={{ scrollSnapAlign: 'start' }}>
-              <RoomJoinSection roomName={rooms.roomName} topic={rooms.topic} occupied={rooms.occupied} capacity={rooms.capacity} />
+          {rooms.map(({ name, topics, occupied, capacity, difficulty, uid }:any, index: any) => (
+            <div style={{ scrollSnapAlign: 'start' }}>
+              <RoomJoinSection
+                name={name}
+                topics={topics}
+                occupied={occupied}
+                capacity={capacity}
+                difficulty={difficulty}
+                uid={uid}
+                key={index}
+              />
             </div>
           ))}
         </div>
       </div>
     </div>
   );
-};
 
-export default JoinRoomCard;
+};
