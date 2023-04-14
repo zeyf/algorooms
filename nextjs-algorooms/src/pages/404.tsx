355e94a23bdc91f70763b6705c3011c841e8f880
zeyf <zainy0012@gmail.com>
2023-04-14 18:56:32 -0400

    Added injectable parameter for dynamic route 404 message support.

--- nextjs-algorooms/src/pages/404.tsx
+++ nextjs-algorooms/src/pages/404.tsx
@@ -1,11 +1,21 @@
-
+import { useRouter } from "next/router";
 
 export default ({
 
-}) => (
-    <div className="h-screen w-screen bg-darkAccent flex justify-center items-center">
-        <div>
-            <span className="text-white">404 - Page Not Found</span>
+}) => {
+
+    const {
+        query: {
+            injectable
+        }
+    } = useRouter();
+
+    return (
+        <div className="h-screen w-screen bg-darkAccent flex justify-center items-center">
+            <div>
+                <span className="text-white text-2xl">{`404 - This${` ${injectable}`} page was not found.`}</span>
+            </div>
         </div>
-    </div>
-);
+    );
+
+};
