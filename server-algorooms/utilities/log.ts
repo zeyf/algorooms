ddecd556c9028aa835417246b393894d6da5cbb9
zeyf <zainy0012@gmail.com>
2023-04-14 16:51:54 -0400

    1. Created a logging function for route request hits for easier and quicker debugging and testing
    - Format = HOUR:MINUTES:MILLISECONDS -> HTTP_METHOD :: PATH
    - Format = KEY : VALUE printing for BODY, PARAMS, & QUERY with corresponding header
    - Separation styling

---
+++ server-algorooms/utilities/log.ts
@@ -0,0 +1,69 @@
+export default function LOG(
+    routeBase: string,
+    {
+        method,
+        path,
+        body,
+        params,
+        query
+    }: any
+):void {
+    
+    // For logging styling for separation
+    const BAR = "======";
+
+    // Lambda to pad time with a 0
+    const formatTime = (integer:number):string => `${integer < 10 ? "0" : ""}${integer}`;
+
+    // Get current date
+    const date = new Date();
+    
+    // Get the current hour in integer
+    let hours: number | string = date.getHours();
+
+    // Get the current period
+    const period = hours < 12 ? "AM" : "PM";
+
+    // Convert hour to string
+    hours = formatTime(date.getHours());
+
+    // Get the current minute and millisecond
+    const minutes = formatTime(date.getMinutes()),
+          milliseconds = formatTime(Math.floor(date.getMilliseconds() / 60));
+
+    // The base logString
+    let logString = `${BAR}\n\n${hours}:${minutes}:${milliseconds} ${period} -> ${method} :: ${routeBase}${path}\n\n`;
+
+    // If we have a body, print its contents
+    if (Object.keys(body).length > 0) {
+        logString += "BODY:\n\n";
+
+        for (const key in body)
+            logString += `${key} : ${body[key]}\n`;
+        
+        logString += "\n";
+    };
+
+    // If we have parameters, add its contents
+    if (Object.keys(params).length > 0) {
+        logString += "PARAMS:\n\n";
+
+        for (const key in params)
+            logString += `${key} : ${params[key]}\n`;
+        
+        logString += "\n";
+    };
+
+    // If we have queries, add its contents
+    if (Object.keys(query).length > 0) {
+        logString += "QUERY:\n\n";
+
+        for (const key in query)
+            logString += `${key} : ${query[key]}\n`;
+        
+        logString += "\n";
+    };
+
+    // Log the contents
+    console.log(`${logString}${BAR}`);
+};
