47c2e5de72dc1bfef44d473af6303c2be84cd3a7
zeyf <zainy0012@gmail.com>
2023-04-15 20:59:07 -0400

    1. Create /api/questions route base
    2. Create /api/questions/filter route
    
    - POST
    - Allows for the OR-based filtering of topic lists of given questions
    
    3. Create /api/questions/create route
    
    - POST
    - Self-explanatory
    
    4. Added logging to both routes

---
+++ server-algorooms/routes/Question.ts
@@ -0,0 +1,87 @@
+import express from "express";
+import Question from "../models/QuestionModel";
+import LOG from "../utilities/log";
+
+const router = express.Router();
+const ROUTE_BASE = "/api/questions";
+
+/*
+*
+* GET routes
+*
+* - /api/questions/filter
+*
+*****
+*
+* POST routes
+*
+* - /api/questions/create
+*
+*/
+
+router.get("/filter", async (req, res) => {
+
+    // For logging and testing
+    LOG(ROUTE_BASE, req);
+
+    // Extract the body
+    const {
+        body: {
+            difficulty,
+            topics
+        }
+    } = req;
+
+
+    await Question.find({
+        difficulty,
+        topics: {
+            "$in" : topics
+        }
+    }).then(response => {
+
+        res.status(200).send({
+            questions: response
+        });
+
+    });
+
+});
+
+router.post("/create", async (req, res) => {
+
+    // For logging and testing
+    LOG(ROUTE_BASE, req);
+
+    // Extract body data
+    const {
+        body: {
+            title,
+            index,
+            difficulty,
+            topics,
+            description,
+            constraints,
+            hints
+        }
+    } = req;
+
+    await Question.create({
+        title,
+        index,
+        difficulty,
+        topics,
+        description,
+        constraints,
+        hints
+    }).then((response) => {
+
+        res.status(200).send({
+            created: true
+        });
+
+    })
+
+});
+
+export default router;
