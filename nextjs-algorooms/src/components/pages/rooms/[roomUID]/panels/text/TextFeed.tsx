820b26df7f22276ea40e849da32c16b07f886297
zeyf <zainy0012@gmail.com>
2023-04-22 15:47:14 -0400

    1. Brought in the RoomContext in order to access the roomMessages 2. Mapping the messages, username, and timestamp (in the form of hours and minutes without 0 padding)

--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/text/TextFeed.tsx
+++ nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/text/TextFeed.tsx
@@ -1,43 +1,48 @@
 // Module imports
-import React from 'react';
+import React, { useContext } from 'react';
 import { useState, useEffect } from 'react';
 // Add this when text chat is working
 // import RoomMember from './header/RoomMember';
 
 // Interface imports
 import { textFeedInterface } from './Interfaces';
+import { RoomContext } from '@/contexts/RoomContextLayer';
 
-export default ({ socket }: textFeedInterface) => {
+export default ({
+  socket
+}: textFeedInterface) => {
   // Code
 
-  //The current feed / messages, test it by putting some string in it, now have name along with it
-  const [messages, setMessages] = useState<String[][]>([[
-    'Khoa',
-    'Lorem Ipsen or something like that',
-  ]
-  ]);
-
-  // Append incoming messages to the array 
-  useEffect(() => {
-    socket.on('message', ({ message, name }: any) => {
-      setMessages([...messages, [name, message]]);
-    });
-  });
+  const {
+    messages
+  } = useContext(RoomContext);
 
   return (
     <section>
       <div className="overflow-y-auto max-h-[400px] bg-darkAccent flex flex-col">
         {/* NOT TESTED */}
-        {messages.map((message, index) => (
-          <div key={index} className="my-2 mx-2">
-            <span className="text-white flex flex-row">
-              <div className="flex-shrink-0 text-gray-400">
-                {message[0]}
+        {
+          messages.map(({
+            message,
+            username,
+            timestamp
+          }, index) => {
+            
+            const date = new Date(timestamp);
+            const hour = date.getHours(),
+                  minutes = date.getMinutes();
+
+            return (
+              <div key={index} className="my-2 mx-2">
+                <span className="text-white flex flex-row">
+                  <div className="flex-shrink-0 text-gray-400">
+                    { `${username}: ${message}\t${hour}:${minutes}` }
+                  </div>
+                </span>
               </div>
-              : {message[1]}
-            </span>
-          </div>
-        ))}
+            )
+          })
+        }
       </div>
     </section>
   );
