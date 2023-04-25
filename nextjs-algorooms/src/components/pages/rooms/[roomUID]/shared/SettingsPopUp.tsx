837439a967ed2fb8389dd6533fb6ae66b6659d87
zeyf <zainy0012@gmail.com>
2023-04-25 18:46:13 -0400

    1. Renamed the file for the SettingsPopUp to follow convention 2. Brought a lot of context functionalities and elements to this level 3. Updated the data state handling and composition in order to effectively change room settings with visual conditional component rendering 4. Added useEffect cleanup handling in the case there IS an actual change to the state of the data relative to the previous value 5. Created a handleSave function that handles the saving of the new state of the room settings
    - Makes a post request to the /api/rooms/update/:roomUID route with the new difficulty, topics, and lobbyAccess
    - This request returns an updated variable, which if set to true:
    	- Effects the state to reflects a successful POST request
    	- Tracking of the changes of the current room settings and the temporary ones in the settings pop up
    	- Communicates any possible changes to the backend socket.io event listener 'backendSettingsChange' with room uid, user name, and socket id
    	- Closes the pop up
    	- Makes a toast for the client sender!
    
    6. Added defaultValue, value, and onChange handling for the multi-select Select tag for the topics
    7. Added the defaultValue, value, and onChange handling for the multi-select Select tag for the difficulty
    
    Learned: Had to bring the socket event listener 'frontendSettingsChange' up a level as I was trying to do a socket.on listen when I was going to cleanup via useEffect on component dismount or when I closed the popup via the boolean state value passed into the component, but this effectively does the same and there would be no awaiting this call, therefore it was unreliable to leave in this component! Had to bring it up as the header is never un-rendered unlike the SettingsPopUp. The more you know! (small details)

---
+++ nextjs-algorooms/src/components/pages/rooms/[roomUID]/shared/SettingsPopUp.tsx
@@ -0,0 +1,207 @@
+import React, { useContext, useEffect } from "react";
+import Select from "react-select";
+import { useState } from "react";
+import { MdContentCopy } from "react-icons/md";
+import axios from "axios";
+import buildRoute from "@/utilities/buildRoute";
+import { RoomContext } from "@/contexts/RoomContextLayer";
+import { toast } from "react-toastify";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
+const options = [
+    { value: "Strings", label: "Strings" },
+    { value: "Arrays", label: "Arrays" },
+    { value: "Stacks", label: "Stacks" },
+    { value: "Queues", label: "Queues" },
+    { value: "Linked Lists", label: "Linked Lists" },
+    { value: "Trees", label: "Trees" },
+    { value: "Tries", label: "Tries" },
+    { value: "Recursion", label: "Recursion" },
+    { value: "Hash Tables", label: "Hash Tables" },
+    { value: "Graphs", label: "Graphs" },
+    { value: "Bitwise", label: "Bitwise" }
+  ];
+
+const optionsDiff = [
+    { value: "Simpler", label: "Simpler" },
+    { value: "Simple", label: "Simple" },
+    { value: "Not Simple", label: "Not Simple" },
+];
+
+const SettingsPopUp = ({
+    isSettingsOpen,
+    setIsSettingsOpen,
+    buildSettingsChangeToastMessage
+}) => {
+
+    const {
+        user
+    } = useUser();
+
+    const {
+        socket,
+        uid,
+
+        topics,
+        setTopics,
+        lobbyAccess,
+        setLobbyAccess,
+        difficulty,
+        setDifficulty
+    } = useContext(RoomContext);
+
+    const [
+        data,
+        setData
+    ] = useState<any>({
+        tempTopics: topics,
+        tempLobbyAccess: lobbyAccess,
+        tempDifficulty: difficulty,
+        trueChange: false
+    });
+
+    useEffect(() => {
+        
+        return () => {
+
+            if (data.trueChange) {
+
+                // Reset the state on dismount
+                setData({
+                    tempTopics: topics,
+                    tempLobbyAccess: lobbyAccess,
+                    tempDifficulty: difficulty,
+                    trueChange: false
+                });
+
+            } else {
+                
+                setData({
+                    ...data,
+                    trueChange: false
+                });
+
+            }
+
+        };
+
+    }, [  ]);
+
+    const setButtonColorOnCondition = (condition:boolean):string => condition ? "bg-darkAccent text-white" : "bg-greenAccent text-darkAccent";
+
+    const handleSave = async e => {
+        e.preventDefault();
+
+        const response = await axios.post(buildRoute(`/api/rooms/update/${uid}`), {
+            difficulty: data.tempDifficulty,
+            topics: data.tempTopics,
+            lobbyAccess: data.tempLobbyAccess
+        }).then(res => res.data);
+
+        const {
+            updated
+        } = response;
+        
+        // Change to test
+        if (updated) {
+
+            setData({
+                ...data,
+                trueChange: true
+            });
+
+            const topicChange = data.tempTopics.sort().toString() !== topics.sort().toString() ? data.tempTopics : null,
+                  difficultyChange = data.tempDifficulty !== difficulty ? data.tempDifficulty : null,
+                  lobbyAccessChange = data.tempLobbyAccess !== lobbyAccess ? data.tempLobbyAccess : null
+
+            socket.emit("backendSettingsChange", {
+                topics: topicChange,
+                difficulty: difficultyChange,
+                lobbyAccess: lobbyAccessChange
+            }, uid, user.name, socket.id);
+
+
+            setIsSettingsOpen(false);
+
+            toast(buildSettingsChangeToastMessage(
+                topicChange,
+                difficultyChange,
+                lobbyAccessChange
+            ));
+
+        };
+
+    };
+
+    return (
+        <div className="w-[270px] h-[300px]">
+            <div className="h-full flex-col rounded-2xl bg-[#222C4A] shadow-xl border-black border-[3px]">
+                <div className="flex flex-col translate-x-[42.5px] translate-y-[35px] justify-center z-50 text-white w-[185px]">
+                    <Select
+                        name="colors"
+                        className="w-full basic-multi-select text-black"
+                        classNamePrefix="select"
+                        isMulti={true}
+                        options={options}
+                        placeholder="Topics"
+                        menuPortalTarget={document.body}
+                        styles={{ menuPortal: (base) => ({ ...base, zIndex: 99 }) }}
+                        defaultValue={topics.map(topic => { return { value: topic, label: topic } })}
+                        value={data.tempTopics.map(topic => { return { value: topic, label: topic } })}
+                        onChange={(selections:any) => setData({ ...data, tempTopics: selections.map((selection:any) => selection.value) })}
+                    />
+                </div>
+                <div className="flex flex-col translate-x-[42.5px] translate-y-[55px] justify-center z-50 text-white w-[185px]">
+                    <Select
+                        name="colors"
+                        className="w-full basic-multi-select text-black"
+                        classNamePrefix="select"
+                        options={optionsDiff}
+                        placeholder="Difficulty"
+                        menuPortalTarget={document.body}
+                        styles={{ menuPortal: (base) => ({ ...base, zIndex: 99 }) }}
+                        defaultValue={difficulty}
+                        value={data.tempDifficulty}
+                        onChange={({ value, label }) => {setData({ ...data, tempDifficulty: value })}}
+                    />
+                </div>
+            </div>
+            <div className="flex flex-col justify-center translate-y-[-140px] translate-x-[45px]">
+                <div>
+                    <button
+                        type="button"
+                        className={`${setButtonColorOnCondition(data.selectedLobbyAccess === "Public")} w-[92px] h-[41px] bg font-bold py-2 rounded-tl-[15px] rounded-bl-[15px]`}
+                        onClick={() => setData({ ...data, tempLobbyAccess: "Public" })}
+                    >
+                        Public
+                    </button>
+                    <button
+                        type="button"
+                        className={`${setButtonColorOnCondition(data.selectedLobbyAccess === "Private")} w-[92px] h-[41px] bg font-bold py-2 rounded-tr-[15px] rounded-br-[15px]`}
+                        onClick={() => setData({ ...data, tempLobbyAccess: "Private" })}
+                    >
+                        Private
+                    </button>
+                </div>
+            </div>
+            <button
+                type="button"
+                className="w-[71px] h-[46px] bg-darkAccent font-bold py-2 rounded-[15px] mt-4 translate-y-[-135px] translate-x-[100px] border-white border-[3px]"
+                onClick={() => {
+                    const url = window.location.href;
+                    navigator.clipboard.writeText(url).then(() => {});
+                }}
+                >
+                    <MdContentCopy className="text-white text-2xl ml-[23px]" />
+            </button>
+
+            <button
+                onClick={handleSave}
+            >
+                Save
+            </button>
+        </div>
+    );
+};
+
+export default SettingsPopUp;
