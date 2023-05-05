1b2344cc27216bb15d7cc0ca3e412323a39ebbc1
Khoi Vu <khoivu@Khois-MacBook-Pro.local>
2023-05-04 20:11:35 -0400

    Implemented YJS CRDT into the code editor

--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/CodeEditor.tsx
+++ nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/CodeEditor.tsx
@@ -1,22 +1,21 @@
 // Module imports
-import React, { useContext } from "react";
+import { useState, useRef, useEffect, useContext } from 'react'
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript as JAVASCRIPT_SYNTAX_HIGHLIGHTING_EXTENSION } from '@codemirror/lang-javascript';
 import { python as PYTHON_SYNTAX_HIGHLIGHTING_EXTENSION } from "@codemirror/lang-python";
 import { cpp as CPP_SYNTAX_HIGHLIGHTING_EXTENSION } from "@codemirror/lang-cpp";
 import { java as JAVA_SYNTAX_HIGHLIGHTING_EXTENSION } from '@codemirror/lang-java';
-
-import { darcula } from '@uiw/codemirror-theme-darcula'
 import { sublime } from '@uiw/codemirror-theme-sublime'
-import { Select, Option } from "@material-tailwind/react";
-import { Button } from "flowbite-react";
-import { useState, useRef, useEffect } from 'react'
+import * as Y from "yjs";
+import { WebrtcProvider } from "y-webrtc";
+import { yCollab } from 'y-codemirror.next'
 
 // Interface imports
 import { codeEditorInterface } from "./Interfaces";
 import { countReset } from "console";
 import rooms from "@/pages/rooms";
 import { RoomContext } from "@/contexts/RoomContextLayer";
+import { AppUserContext } from '@/contexts/AppUserContextLayer';
 
 export default ({
 
@@ -30,6 +29,10 @@ export default ({
         language
     } = useContext(RoomContext);
 
+    const {
+        username
+    } = useContext(AppUserContext);
+
     const languageMapping = [
         [ "python", PYTHON_SYNTAX_HIGHLIGHTING_EXTENSION ],
         [ "javascript", JAVASCRIPT_SYNTAX_HIGHLIGHTING_EXTENSION ],
@@ -38,28 +41,50 @@ export default ({
     ];
 
     // Code
-    const onChange = React.useCallback((value:any, viewUpdate:any) => {
-        socket.emit("codeChange", value, uid);
 
-    }, [  ]);
+    // const onChange = React.useCallback((value:any, viewUpdate:any) => {
+    //     socket.emit("codeChange", value, uid);
 
-    socket.on("updateEditor", (arg:any) => {
-        setCode(arg);
-    });
+    // }, [  ]);
 
+    // socket.on("updateEditor", (arg:any) => {
+    //     setCode(arg);
+    // });
+
+    const [collabRef, setCollabRef] = useState(null)
+
+    useEffect(() => {
+        const ydoc = new Y.Doc();
+        const provider = new WebrtcProvider(uid, ydoc);
+        const yText = ydoc.getText("codemirror");
+        const awareness = provider.awareness;
+        awareness.setLocalStateField("user", {
+            name: username
+        })
+        const undoManager = new Y.UndoManager(yText);
+        setCollabRef(yCollab(yText, awareness, {undoManager}))
+
+        return () => {
+            if (provider) {
+                provider.disconnect();
+                ydoc.destroy();
+            }
+        }
+    }, [])
+  
 
     return (
         <div className="w-[822px] h-[649px]">
             <div className="rounded-lg overflow-hidden w-[822px] h-[579px] mt-5">
                 <CodeMirror
-                    value = {code}
-                    onChange={onChange}
                     height="579px"
                     width="822px"
-                    extensions={
+                    extensions={[
                         languageMapping
                         .filter(([ lang, ext ]:any[]) => language === lang)
-                        .map(([ lang, ext ]:any[]) => ext())
+                        .map(([ lang, ext ]:any[]) => ext()),
+                        collabRef
+                    ]
                     }
                     theme={sublime}
                 />
