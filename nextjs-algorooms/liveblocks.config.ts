60a2703efd6f359dbdb5fb74eeb53df1a34c7a9a
zeyf <zainy0012@gmail.com>
2023-05-09 02:48:15 -0400

    1. Installed @liveblocks/react, @liveblocks/client, @liveblocks/node
    2. Created client
    
    3. Defined presence, textChatMessage, and Storage types for LiveBlocks (for rooms)

---
+++ nextjs-algorooms/liveblocks.config.ts
@@ -0,0 +1,50 @@
+import { LiveList, LiveObject, createClient } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+const client = createClient({
+  publicApiKey: "pk_dev_7rSbiCxmktdPPA-hgYZsTSxKsm3d3WUtHWKcq8wYTBLEV6lJKnWHGmEkZzEGsfMS",
+});
+
+export type Presence = {
+    isTypingCode: boolean,
+    isTypingMessage: boolean,
+    isRunningCode: boolean,
+    isSubmittingCode: boolean,
+    username: string,
+    color: string,
+    cursorLocationData: any
+};
+
+type TextChatMessage = {
+    username: string,
+    message: string,
+    timestamp: number
+};
+
+type Storage = {
+    uid: string,
+    editorText: string,
+    lobbyAccess: string,
+    difficulty: string,
+    topics: LiveList<string>,
+    members: LiveList<string>,
+    messages: LiveList<string>,
+    host: string
+};
+
+// export const initialPresence = {
+//     isTypingCode: false,
+//     isTypingMessage: false,
+//     isRunningCode: false,
+//     isSubmittingCode: false,
+//     username: ""
+// };
+
+export const {
+    RoomProvider,
+    useOthers,
+    useStorage,
+    useUpdateMyPresence,
+    useMutation,
+    useSelf
+} = createRoomContext<Presence, Storage>(client);
