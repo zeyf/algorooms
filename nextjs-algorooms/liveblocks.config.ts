c768683a77ed4b79b8d3adbc07748c42f742ae01
zeyf <zainy0012@gmail.com>
2023-05-13 15:52:50 -0400

    1. Commented out the LB configuration

--- nextjs-algorooms/liveblocks.config.ts
+++ nextjs-algorooms/liveblocks.config.ts
@@ -1,10 +1,18 @@
-import { LiveList, LiveObject, createClient } from "@liveblocks/client";
-import { createRoomContext } from "@liveblocks/react";
+import {
+    LiveList,
+    createClient
+} from "@liveblocks/client";
 
+import {
+    createRoomContext
+} from "@liveblocks/react";
+
+// Create the client connection to LiveBlocks
 export const client = createClient({
   publicApiKey: "pk_dev_7rSbiCxmktdPPA-hgYZsTSxKsm3d3WUtHWKcq8wYTBLEV6lJKnWHGmEkZzEGsfMS",
 });
 
+// Type schema for presence -- a user's own unique state that is shared
 export type Presence = {
     isTypingCode: boolean,
     isTypingMessage: boolean,
@@ -16,13 +24,7 @@ export type Presence = {
     joined: number
 };
 
-export type TextChatMessage = {
-    username: string,
-    message: string,
-    timestamp: number,
-    color: string
-};
-
+// Type schema for storage -- non-unique shared data
 export type Storage = {
     uid: string,
     editorText: string,
@@ -41,6 +43,15 @@ export type Storage = {
     currentQuestion: any // to be question object
 };
 
+// Type schema for text chat messages
+export type TextChatMessage = {
+    username: string,
+    message: string,
+    timestamp: number,
+    color: string
+};
+
+// Access to context provider and hooks
 export const {
     suspense: {
         RoomProvider,
