98b12710ae3e5b918188a20e7fa254b4460c0b0b
Khoivu166 <khoivu166@gmail.com>
2023-05-13 20:52:04 -0400

    Added input, output, explanation to scraper and difficulty

--- server-algorooms/scraper/scraper.py
+++ server-algorooms/scraper/scraper.py
@@ -1,17 +1,17 @@
 import requests
 import json
+import re;
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-
+# open the questions url json
 f = open('leetcode_questions.json')
 data = json.load(f)
 
+# store all the urls in an array
 urlArray = []
 for question in data:
-    urlArray.append((question['title'], question['url']))
-
-
+    urlArray.append((question['title'], question['url'], question['difficulty']))
 
 # Path to the chromedriver executable
 chromedriver_path = 'usr/local/bin/chromedriver.exe'
@@ -23,36 +23,77 @@ driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
 
 count = 0
 questions = []
-for title, url in urlArray:
-    if count == 20:
-        break
+for title, url, difficulty in urlArray:
+    # if count == 150:
+    #     break
+
     # Load the question page using the webdriver
     driver.get(url)
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     
     # Get the question description and example inputs/outputs
-    print("title: " + title)
-    description = soup.find('div', {'class': '_1l1MA'}).get_text().strip()
+    print(f"#{count + 1}\ttitle: {title}")
+    mainContainer = soup.find('div', {'class': '_1l1MA'})
+    description = mainContainer.get_text().strip()
+    examples = [  ];
+    pre_tags = mainContainer.find_all('pre')
+
+
+    inputRegex, outputRegex, explanationRegex = r"Input(\n|: ).*", r"Output(\n|: ).*", r"Explanation(\n|: ).*"; 
+    for ex in pre_tags:
+        exampleText = ex.get_text();
+
+        inputText = re.search(inputRegex, exampleText);
+        inputText = "" if inputText == None else inputText.group();
+
+        outputText = re.search(outputRegex, exampleText);
+        outputText = "" if outputText == None else outputText.group();
+
+        explanationText = re.search(explanationRegex, exampleText);
+        explanationText = "" if explanationText == None else explanationText.group();
+
+        examples.append({
+            "input": inputText,
+            "output": outputText,
+            "explanation": explanationText
+        });
+
+
+    
     topics = []
 
-    # allA = [  soup.find_all("a") ]
-
+    # find div that contains the topic tags
     tag = soup.find('div', {'class': 'mt-2 flex flex-wrap gap-y-3'})
     if tag != None:
-        # tempTag = tag.get_text().strip()
-        # print(tempTag)
-        # topics.append(tempTag)
+        # create an array that finds all the a tags that have an href
         tempTags = tag.find_all("a", href=True)
-        for i in tempTags:
-            topics.append(i.get_text())
-        
+        for thing in tempTags:
+            # append the text of the a tag into the topics array
+            topics.append(thing.get_text())
+
     
-    # Add the question information to the list of questions
-    questions.append({
-        'title': title,
-        'description': description,
-        'topics': topics
-    })
+    def translateDifficulty(score):
+        if score == 1:
+            return "Easy";
+        elif score == 2:
+            return "Medium";
+        return "Hard";
+    
+    ## If we have at least one topic
+    if len(topics) > 0:
+        # Add the question information to the list of questions
+        questions.append({
+            'title': title,
+            'description': description,
+            'examples': examples,
+            'topics': topics,
+            "difficulty": translateDifficulty(difficulty)
+        });
+    
+
+
+
+
     count += 1
 
 # Save the questions to a JSON file
