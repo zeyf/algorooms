6c020fc9b3d4aad21667292fbc6fa9cf7744922f
zeyf <zainy0012@gmail.com>
2023-05-15 14:57:49 -0400

    1. Commented and cleaned up the TextFeed component

--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/text/TextFeed.tsx
+++ nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/text/TextFeed.tsx
@@ -1,12 +1,9 @@
 // Module imports
-import React, { useContext } from 'react';
-import { useState, useEffect, useRef } from 'react';
-// Add this when text chat is working
-// import RoomMember from './header/RoomMember';
+import React from 'react';
+import { useEffect, useRef } from 'react';
 
 // Interface imports
 import { textFeedInterface } from './Interfaces';
-import { RoomContext } from '@/contexts/RoomContextLayer';
 import { useStorage } from '../../../../../../../liveblocks.config';
 import formatTime from '@/utilities/formatTime';
 
@@ -14,34 +11,45 @@ export default ({
 
 }: textFeedInterface) => {
 
-  const messages = useStorage(root => root.messages) || [  ];
+  // Read in all messages
+  const messages = useStorage(root => root.messages);
 
+  // Create reference for the messages div
   const containerRef = useRef(null);
 
   // This is to make sure the scroll bar scroll down when new messages got added
   useEffect(() => {
+
+    // Get the reference to the div
     const container = containerRef.current;
 
+    // Are we at the very bottom of the div
     const isBottom = container.scrollHeight - container.scrollTop - 40 === container.clientHeight;
 
+    // Are we at the very top of the div
     const isTop = container.scrollTop === 0;
 
-    if(isBottom || isTop) {
+    // If we are at the bottom or at the top, scroll to the very bottom
+    if (isBottom || isTop)
       container.scrollTo(0, container.scrollHeight);
-    }
+
+  // The messages length is a dependency for the useEffect as we want to scroll down on addition of every new message
   }, [ messages.length ]);
 
   return (
     <section>
       <div className="overflow-y-auto max-h-[400px] bg-darkAccent flex flex-col" ref={containerRef}>
-        {/* NOT TESTED */}
         {
+
+          // Display all messages
+
           messages.map(({
             message,
             username,
             timestamp
           }, index) => {
             
+            // Get the hour and time from the timestamp
             const date = new Date(timestamp);
             const hour = date.getHours(),
                   minutes = date.getMinutes();
