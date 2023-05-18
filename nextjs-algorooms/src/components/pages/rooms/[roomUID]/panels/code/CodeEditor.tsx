31ea33330a8971b23a06dec51687bd7651099023
James Nguyen <hjaenscode@gmail.com>
2023-05-17 20:09:29 -0400

    Made CodeEditor scrollable

--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/CodeEditor.tsx
+++ nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/CodeEditor.tsx
@@ -1,185 +1,166 @@
 // Module imports
-import { useState, useRef, useEffect, useContext } from 'react'
+import { useState, useRef, useEffect, useContext } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { javascript as JAVASCRIPT_SYNTAX_HIGHLIGHTING_EXTENSION } from '@codemirror/lang-javascript';
-import { python as PYTHON_SYNTAX_HIGHLIGHTING_EXTENSION } from "@codemirror/lang-python";
-import { cpp as CPP_SYNTAX_HIGHLIGHTING_EXTENSION } from "@codemirror/lang-cpp";
+import { python as PYTHON_SYNTAX_HIGHLIGHTING_EXTENSION } from '@codemirror/lang-python';
+import { cpp as CPP_SYNTAX_HIGHLIGHTING_EXTENSION } from '@codemirror/lang-cpp';
 import { java as JAVA_SYNTAX_HIGHLIGHTING_EXTENSION } from '@codemirror/lang-java';
-import { sublime } from '@uiw/codemirror-theme-sublime'
-import * as Y from "yjs";
-import { WebrtcProvider } from "y-webrtc";
-import { yCollab } from 'y-codemirror.next'
+import { sublime } from '@uiw/codemirror-theme-sublime';
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+import { yCollab } from 'y-codemirror.next';
 
 // Interface imports
-import { codeEditorInterface } from "./Interfaces";
-import { countReset } from "console";
-import rooms from "@/pages/rooms";
-import { RoomContext } from "@/contexts/RoomContextLayer";
+import { codeEditorInterface } from './Interfaces';
+import { countReset } from 'console';
+import rooms from '@/pages/rooms';
+import { RoomContext } from '@/contexts/RoomContextLayer';
 import { AppUserContext } from '@/contexts/AppUserContextLayer';
-import { useMutation, useOthers, useSelf, useStorage, useUpdateMyPresence } from '../../../../../../../liveblocks.config';
+import {
+  useMutation,
+  useOthers,
+  useSelf,
+  useStorage,
+  useUpdateMyPresence,
+} from '../../../../../../../liveblocks.config';
 import { EditorState } from '@codemirror/state';
-import {Tooltip, showTooltip, EditorView} from "@codemirror/view"
-import {StateField} from "@codemirror/state"
+import { Tooltip, showTooltip, EditorView } from '@codemirror/view';
+import { StateField } from '@codemirror/state';
 
-import randomColor from "randomcolor";
-import Split from 'react-split';
-import CodeTester from './CodeTester';
+const CodeEditor = ({}: codeEditorInterface) => {
+  const { socket, uid, code, setCode, language } = useContext(RoomContext);
 
+  const { username } = useContext(AppUserContext);
 
+  const editorText = useStorage(({ editorText }) => editorText) || '';
 
-const CodeEditor = ({
+  const languageMapping = [
+    ['python', PYTHON_SYNTAX_HIGHLIGHTING_EXTENSION],
+    ['javascript', JAVASCRIPT_SYNTAX_HIGHLIGHTING_EXTENSION],
+    ['cpp', CPP_SYNTAX_HIGHLIGHTING_EXTENSION],
+    ['java', JAVA_SYNTAX_HIGHLIGHTING_EXTENSION],
+  ];
 
-}) => {
+  const handleEditorTextEdit = useMutation(({ storage }, newEditorText) => {
+    storage.set('editorText', newEditorText);
+  }, []);
 
-    const {
-        socket,
-        uid,
-        code,
-        setCode,
-        language
-    } = useContext(RoomContext);
+  const myPresence = useSelf((me) => me.presence);
 
-    const {
-        username
-    } = useContext(AppUserContext);
+  const others = useOthers();
 
-    const editorText = useStorage(({ editorText }) => editorText) || "";
+  const handleEditorPositionUpdate = useUpdateMyPresence();
 
-    const languageMapping = [
-        [ "python", PYTHON_SYNTAX_HIGHLIGHTING_EXTENSION ],
-        [ "javascript", JAVASCRIPT_SYNTAX_HIGHLIGHTING_EXTENSION ],
-        [ "cpp", CPP_SYNTAX_HIGHLIGHTING_EXTENSION ],
-        [ "java", JAVA_SYNTAX_HIGHLIGHTING_EXTENSION ]
-    ];
+  function getCursorTooltips(state: EditorState): readonly Tooltip[] {
+    const otherTooltips = others.map((other) => {
+      return {
+        cursorLocationData: other.presence.cursorLocationData,
+        username: other.presence.username,
+        color: other.presence.color,
+      };
+    });
 
-    const handleEditorTextEdit = useMutation(({ storage }, newEditorText) => {
-        storage.set("editorText", newEditorText);
-    }, [  ]);
+    const myTooltip = {
+      cursorLocationData: myPresence.cursorLocationData,
+      username: myPresence.username,
+      color: myPresence.color,
+    };
 
-    const myPresence = useSelf(me => me.presence);
-    
-    const others = useOthers();
-    
-    const handleEditorPositionUpdate = useUpdateMyPresence();
+    return [
+      ...otherTooltips,
+      // myTooltip
+    ]
+      .filter((userTooltipState) => userTooltipState.cursorLocationData.empty)
+      .map((userTooltipState) => {
+        // let line = state.doc.lineAt(range.head);
 
-    function getCursorTooltips(state: EditorState): readonly Tooltip[] {
+        return {
+          pos: userTooltipState.cursorLocationData.head,
+          above: true,
+          strictSide: true,
+          arrow: true,
+          create: () => {
+            let dom = document.createElement('div');
+            dom.className = 'cm-tooltip-cursor';
+            dom.textContent = userTooltipState.username;
+            dom.style.backgroundColor = userTooltipState.color;
 
-        const otherTooltips = others.map(other => {
             return {
-                cursorLocationData: other.presence.cursorLocationData,
-                username: other.presence.username,
-                color: other.presence.color
-            }
-        });
-
-        const myTooltip = {
-            cursorLocationData: myPresence.cursorLocationData,
-            username: myPresence.username,
-            color: myPresence.color
-        };
-
-        return [
-            ...otherTooltips,
-            // myTooltip
-        ]
-          .filter((userTooltipState) => userTooltipState.cursorLocationData.empty)
-          .map((userTooltipState) => {
-            // let line = state.doc.lineAt(range.head);
-        
-            return {
-              pos: userTooltipState.cursorLocationData.head,
-              above: true,
-              strictSide: true,
-              arrow: true,
-              create: () => {
-                let dom = document.createElement("div")
-                dom.className = "cm-tooltip-cursor"
-                dom.textContent = userTooltipState.username;
-                dom.style.backgroundColor = userTooltipState.color;
-
-                return {
-                    dom
-                };
-
-              }
-            }
-          })
-      }
-    
-      const cursorTooltipField = StateField.define<readonly Tooltip[]>({
-        create: getCursorTooltips,
-      
-        update(tooltips, tr) {
-          if (!tr.docChanged && !tr.selection) return tooltips
-          return getCursorTooltips(tr.state)
-        },
-      
-        provide: f => showTooltip.computeN([f], state => state.field(f))
-      })
-
-      const cursorTooltipBaseTheme = EditorView.theme({
-        ".cm-tooltip.cm-tooltip-cursor": {
-        //   backgroundColor: c,
-          color: "white",
-          border: "none",
-          padding: "2px 7px",
-          borderRadius: "4px",
-          "& .cm-tooltip-arrow:before": {
-            borderTopColor: "white"
+              dom,
+            };
           },
-          "& .cm-tooltip-arrow:after": {
-            borderTopColor: "transparent"
-          }
-        }
+        };
       });
+  }
 
-    return (
-        <div className="w-full h-full">
-            <div
-              className="rounded-lg overflow-hidden w-full h-full mt-5 bg-[#303841]">
-                <Split className="w-full">
-                    <CodeMirror
-                        height="100%"
-                        width="100%"
-                        
-                        style={{
-                            borderLeft: "1px",
-                            borderLeftColor: myPresence.color,
-                            background: myPresence.color
-                        }}
+  const cursorTooltipField = StateField.define<readonly Tooltip[]>({
+    create: getCursorTooltips,
 
-                        theme={sublime}
-                        value={editorText}
-                        extensions={[
-                            ...languageMapping
-                            .filter(([ lang, ext ]:any[]) => language === lang)
-                            .map(([ lang, ext ]:any[]) => ext()),
-                            cursorTooltipField,
-                            cursorTooltipBaseTheme
-                        ]}
+    update(tooltips, tr) {
+      if (!tr.docChanged && !tr.selection) return tooltips;
+      return getCursorTooltips(tr.state);
+    },
 
-                        onUpdate={viewUpdate => {
-                            const currentData = viewUpdate.state.selection.ranges[0];
+    provide: (f) => showTooltip.computeN([f], (state) => state.field(f)),
+  });
 
-                            if (myPresence.cursorLocationData.from !== currentData.from || myPresence.cursorLocationData.to !== currentData.to) {
+  const cursorTooltipBaseTheme = EditorView.theme({
+    '.cm-tooltip.cm-tooltip-cursor': {
+      //   backgroundColor: c,
+      color: 'white',
+      border: 'none',
+      padding: '2px 7px',
+      borderRadius: '4px',
+      '& .cm-tooltip-arrow:before': {
+        borderTopColor: 'white',
+      },
+      '& .cm-tooltip-arrow:after': {
+        borderTopColor: 'transparent',
+      },
+    },
+  });
 
-                                handleEditorPositionUpdate({ ...myPresence, cursorLocationData: {
-                                    ...currentData,
-                                    empty: currentData.empty,
-                                    head: currentData.head
-                                } });
+  return (
+    <div className="w-full h-full">
+      <div className="rounded-lg overflow-auto w-full h-full bg-[#303841]">
+        <CodeMirror
+          height="100%"
+          style={{
+            borderLeft: '1px',
+            borderLeftColor: myPresence.color,
+            background: myPresence.color,
+          }}
+          theme={sublime}
+          value={editorText}
+          extensions={[
+            ...languageMapping
+              .filter(([lang, ext]: any[]) => language === lang)
+              .map(([lang, ext]: any[]) => ext()),
+            cursorTooltipField,
+            cursorTooltipBaseTheme,
+          ]}
+          onUpdate={(viewUpdate) => {
+            const currentData = viewUpdate.state.selection.ranges[0];
 
-                        }
-                    }}
-
-                    onChange={handleEditorTextEdit}
-                />
-                </Split>
-            </div>
-
-        </div>
-    );
-
+            if (
+              myPresence.cursorLocationData.from !== currentData.from ||
+              myPresence.cursorLocationData.to !== currentData.to
+            ) {
+              handleEditorPositionUpdate({
+                ...myPresence,
+                cursorLocationData: {
+                  ...currentData,
+                  empty: currentData.empty,
+                  head: currentData.head,
+                },
+              });
+            }
+          }}
+          onChange={handleEditorTextEdit}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default CodeEditor;
--- nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/CodePanel.tsx
+++ nextjs-algorooms/src/components/pages/rooms/[roomUID]/panels/code/CodePanel.tsx
@@ -1,38 +1,34 @@
 // Module imports
-import React from "react";
-import Header from "./header/Header";
-import dynamic from "next/dynamic";
-import CodeTester from "./CodeTester";
-const CodeEditor = dynamic(() => import('./CodeEditor'), {ssr:false})
+import React from 'react';
+import Header from './header/Header';
+import dynamic from 'next/dynamic';
+import CodeTester from './CodeTester';
+const CodeEditor = dynamic(() => import('./CodeEditor'), { ssr: false });
 import Split from 'react-split';
-
-
 
 // Interface imports
 
+export default ({}) => {
+  // Code
 
-
-
-export default ({
-
-}) => {
-
-    // Code
-
-    return (
-        <section className="w-full">
-            <Header />
-            <Split sizes={[70,30]} minSize={[300, 0]} direction="vertical" cursor="row-resize" className="w-full"
-              style={{height: "calc(100% - 129px"}}
-            >
-              <div>
-                <CodeEditor />
-              </div>
-              <div>
-                <CodeTester />
-              </div>
-            </Split>
-        </section>
-    );
-
+  return (
+    <section className="w-full">
+      <Header />
+      <Split
+        sizes={[70, 30]}
+        minSize={[300, 0]}
+        direction="vertical"
+        cursor="row-resize"
+        className="w-full"
+        style={{ height: 'calc(100% - 110px' }}
+      >
+        <div className="overflow-auto">
+          <CodeEditor />
+        </div>
+        <div>
+          <CodeTester />
+        </div>
+      </Split>
+    </section>
+  );
 };
