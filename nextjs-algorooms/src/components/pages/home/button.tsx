e40a889426821a38a5e2142608d125ee9528715a
James Nguyen <hjaenscode@gmail.com>
2023-10-08 18:06:58 -0400

    Made landing page more responsive

--- nextjs-algorooms/src/components/pages/home/button.tsx
+++ nextjs-algorooms/src/components/pages/home/button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button } from "flowbite-react";
 const Buttons = () => {
     return(
-        <div>
+        <div className="z-10 flex justify-center items-center">
             <Button className="w-[140px]" href="/api/auth/login">
                 Get Started
             </Button>
